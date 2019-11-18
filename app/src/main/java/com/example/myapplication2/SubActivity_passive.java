2731dc676efe6fb0807f39d5b30d7c62fa256241
tjswls0424 <49965898+tjswls0424@users.noreply.github.com>
2019-11-18 21:10:10 +0900

    test
    test

--- app/src/main/java/com/example/myapplication2/SubActivity_passive.java
+++ app/src/main/java/com/example/myapplication2/SubActivity_passive.java
@@ -2,8 +2,10 @@ package com.example.myapplication2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -24,6 +26,8 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.InputStream;
+
 //  Color UI
 //public class SubActivity_passive<ColorPicker> extends AppCompatActivity implements View.OnClickListener, ColorPickerDialog.OnColorChangedListener{
     public class SubActivity_passive extends  AppCompatActivity {
@@ -34,58 +38,87 @@ import android.widget.Toast;
         private ImageView imageView;
         private TextView textView;
         private Bitmap bitmap;
-        private ImageView_bitmap cv;
+
+    @SuppressLint("ClickableViewAccessibility")
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub_passive);
-        //setContentView(new ImageView_bitmap(this));
-
-        LinearLayout linear1 = (LinearLayout)findViewById(R.id.layout);
-        ImageView_bitmap ibp = new ImageView_bitmap(linear1.getContext());
-        linear1.addView(ibp);
 //        <!--  Color UI  -->
 //        relative_color = (RelativeLayout) findViewById(R.id.relative_color);
 //        btn_colorPicker = (Button) findViewById(R.id.btn_colorPicker);
 //        btn_colorPicker.setOnClickListener(this);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        AssetManager as = getResources().getAssets();
+        InputStream is =null;
+
+        try{
+            is=as.open("colorpalette.png");
+            bitmap = BitmapFactory.decodeStream(is);
+            imageView.setImageBitmap(bitmap);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                view.performClick();
+
+                int x = (int) motionEvent.getX();
+                int y = (int) motionEvent.getY();
+                int pixel = bitmap.getPixel(x, y);
+
+                int red = Color.red(pixel);
+                int blue = Color.blue(pixel);
+                int green = Color.green(pixel);
+
+                Toast.makeText(SubActivity_passive.this,""+red+"/"+blue+"/"+green,Toast.LENGTH_SHORT).show();
+
+                return true;
+            }
+        });
+
     }
 
-    public class ImageView_bitmap extends View
-    {
-
-        public ImageView_bitmap(Context context, AttributeSet attrs)
-        {
-            super(context, attrs);
-        }
-
-        protected void onDraw(Canvas canvas)
-        {
-            int width = canvas.getWidth();
-            int height = canvas.getHeight();
-
-            bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.givemergb);
-            Bitmap resize_bitmap = Bitmap.createScaledBitmap(bitmap, width, height,true);
-
-            canvas.drawBitmap(resize_bitmap,0,0,null);
-        }
-
-//        @Override
-//        public boolean onTouchEvent(MotionEvent motionEvent)
+//    public class ImageView_bitmap extends View
+//    {
+//
+//        public ImageView_bitmap(Context context)
 //        {
-//            if(motionEvent.getAction() == MotionEvent.ACTION_DOWN || motionEvent.getAction() == MotionEvent.ACTION_MOVE)
-//            {
-//                int pixel = bitmap.getPixel((int) motionEvent.getX(), (int) motionEvent.getY());
-//
-//                int r = Color.red(pixel);
-//                int g = Color.green(pixel);
-//                int b= Color.blue(pixel);
-//
-//                Toast.makeText(SubActivity_passive.this,r + "," + g + "," + b,Toast.LENGTH_LONG).show();
-//            }
-//            return true;
+//            super(context);
 //        }
-    }
+//
+//        protected void onDraw(Canvas canvas)
+//        {
+//            int width = canvas.getWidth();
+//            int height = canvas.getHeight();
+//
+//            bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.givemergb);
+////            Bitmap resize_bitmap = Bitmap.createScaledBitmap(bitmap, width, height,true);
+////
+////            canvas.drawBitmap(resize_bitmap,0,0,null);
+//        }
+//
+////        @Override
+////        public boolean onTouchEvent(MotionEvent motionEvent)
+////        {
+////            if(motionEvent.getAction() == MotionEvent.ACTION_DOWN || motionEvent.getAction() == MotionEvent.ACTION_MOVE)
+////            {
+////                int pixel = bitmap.getPixel((int) motionEvent.getX(), (int) motionEvent.getY());
+////
+////                int r = Color.red(pixel);
+////                int g = Color.green(pixel);
+////                int b= Color.blue(pixel);
+////
+////                Toast.makeText(SubActivity_passive.this,r + "," + g + "," + b,Toast.LENGTH_LONG).show();
+////            }
+////            return true;
+////        }
+//    }
 
     public void passiveOnClick(View v) {
         finish();
